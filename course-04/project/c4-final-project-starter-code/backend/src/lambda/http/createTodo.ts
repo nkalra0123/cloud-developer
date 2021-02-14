07ea8a527e0ac31473e3e8a6cce1463f8d1d6801
nkalra0123 <nkalra0123@gmail.com>
2021-02-14 18:48:47 +0530

    Using try catch and returing correct http code for createTodo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -6,23 +6,38 @@ import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import {createTodo} from "../../businessLogic/todos";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
-  const authorization = event.headers.Authorization
-  const split = authorization.split(' ')
-  const jwtToken = split[1]
-
   console.log('Processing event: ', event)
+  try {
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    const authorization = event.headers.Authorization
+    const split = authorization.split(' ')
+    const jwtToken = split[1]
 
-  const todos = await createTodo(newTodo, jwtToken)
+    const todos = await createTodo(newTodo, jwtToken)
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      items: todos
-    })
+    console.log(todos)
+
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        item: {
+          ...todos,
+        }
+      })
+    }
+  }
+  catch(e){
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({e})
+    }
   }
 
   // TODO: Implement creating a new TODO item
