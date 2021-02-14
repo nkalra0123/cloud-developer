fe725b0701011c0e6cd8b546ca823ab736b2d8c8
nkalra0123 <nkalra0123@gmail.com>
2021-02-14 21:12:38 +0530

    updating config with the backend url and auth0 clientid

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,10 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'uhpyuzqbs2'
+const region = 'ap-south-1'
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'nkalra0123.us.auth0.com',            // Auth0 domain
+  clientId: 'kkbrtDcP9BxEIPrCZdesINyERgZ3LYiB',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
